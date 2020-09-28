9e8062c5f83ea581f70471d2d62d30e2c47216ee
matiesclaesen-Intellji <maties.claesen@student.uhasselt.be>
2020-09-28 17:57:09 +0200

    "opdracht_02_04 beide 7&9 zijn afgerond"

--- Semester 1/OGP2/Exercises/Les 1/Intellji/opdracht_02_04/src/com/company/Main.java
+++ Semester 1/OGP2/Exercises/Les 1/Intellji/opdracht_02_04/src/com/company/Main.java
@@ -1,13 +1,12 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
+class print25Ints {
+    public void print() {
         int prev = 0;
         System.out.println("num:" + prev);
         for (int i = 0; i < 25; i++) {
             int now = (int) (Math.random() * 1000);
-	        if (now > prev) {
+            if (now > prev) {
                 System.out.println("num:" + now + "\t\tgreater than");
             } else if (now == prev) {
                 System.out.println("num:" + now + "\t\tequal to");
@@ -16,5 +15,30 @@ public class Main {
             }
             prev = now;
         }
+    }
+}
+
+class findPrimes {
+    public void find(int max) {
+        for(int i = max; i > 1; i--) {
+            boolean isPrime = true;
+            for (int j = i-1; j > 1; j--) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                System.out.println(i);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        print25Ints objOne = new print25Ints();
+        objOne.print();
+        findPrimes objTwo = new findPrimes();
+        objTwo.find(100);
     }
 }
